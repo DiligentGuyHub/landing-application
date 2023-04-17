de0b8ed540a4da87519afa7f28e755f466562432
Viktor <dev.viktor.demidov@gmail.com>
2023-04-17 18:55:24 +0300

    Modify question structure [Server] Remove unnecessary fields and 'required' flags, add fields for min/max in range questions, add validator field for regex, add isActive boolean field

--- server/models/question.js
+++ server/models/question.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const answerSchema = new Schema({
-    text: {type: String, required: true},
-    value: {type: String, required: true}
+    text: {type: String}
 });
 
 const questionSchema = new Schema({
+    _id: {type: String},
     orderId: {type: Number, unique: true},
     text: {type: String, required: true},
     type: {type: String, enum: ['radio', 'checkbox', 'text', 'range'], required: true},
-    field: {type: String, enum: ['firstname', 'lastname', 'email', 'age', 'frequency', 'gender', 'children', 'employment', 'none'], default: 'none'},
-    answers: [answerSchema]
+    field: {type: String, enum: ['name', 'email', 'age', 'frequency', 'gender', 'children', 'employment', 'none'], default: 'none'},
+    answers: [answerSchema],
+    validator: {type: String},
+    min: {type: Number},
+    max: {type: Number},
+    isActive: {type: Boolean, default: true}
 });
 
 const Question = mongoose.model('Question', questionSchema);
